9b33fa7aadce8e38237992e646d60fd5baed5405
Maggie <maggielu2024@gmail.com>
2026-09-10 22:54:12 -0400

    Add week 1 playground

--- week1.playground/Contents.swift
+++ week1.playground/Contents.swift
@@ -1,3 +1,39 @@
 import UIKit
 
-var greeting = "Hello, playground"
+var greeting = "hi i love apple juice" // this is a variable
+greeting = "no like trust. the one from japan"
+var name = "Ted"
+name = "Rebecca"
+name = "Keeley"
+
+let charachter = "Daphne" // this is a constant
+
+var playerName = "Roy"
+print (playerName)
+
+playerName = "Dani"
+print(playerName)
+
+playerName = "Sam"
+print(playerName)
+
+let managerName = "Bill Nye"
+let dogBreed = "Samoyed"
+
+// use constants over variables
+
+let actor = "denzel washington"
+let filename = "paris.jpg"
+let result = " You win!"
+
+let movie = """
+A day in
+the life of an
+Apple juice
+"""
+print(actor.count)
+
+let actorLength = actor.count
+print(actorLength)
+
+print(result.uppercased())
