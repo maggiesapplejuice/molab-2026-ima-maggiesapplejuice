3644e3d7369b4adff3075fb314c8101c65e9f748
Maggie <maggielu2024@gmail.com>
2026-09-19 20:56:49 -0400

    in class work

---
+++ Artermis V2/Artermis V2/Artermis_V2App.swift
@@ -0,0 +1,17 @@
+//
+//  Artermis_V2App.swift
+//  Artermis V2
+//
+//  Created by Maggie Lu on 9/18/26.
+//
+
+import SwiftUI
+
+@main
+struct Artermis_V2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Artermis V2/week3/week3/week3App.swift
@@ -0,0 +1,17 @@
+//
+//  week3App.swift
+//  week3
+//
+//  Created by Maggie Lu on 9/18/26.
+//
+
+import SwiftUI
+
+@main
+struct week3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
